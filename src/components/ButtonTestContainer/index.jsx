58d4a3ad4e1c66de191073f50b4ccd4522bf1a56
Konstantin Basmanov <basmanovk@gmail.com>
2022-04-03 16:22:47 -0600

    Refactor after rereading the instructions.

--- src/components/ButtonTestContainer/index.jsx
+++ src/components/ButtonTestContainer/index.jsx
@@ -1,9 +1,55 @@
+import { useState } from 'react'
 import FavoriteButtonWithState from 'components/FavoriteButtonWithState'
 
+// Foreign key type can be changed to 'userId', 'postId', 'commentId', etc...
+const fk = { userId: '123' }
+
+const initialState = [
+  { id: 1, isOn: false, fk: null },
+  { id: 2, isOn: false, fk: null },
+  { id: 3, isOn: false, fk: null },
+]
+
+// Need this to prevent buttons states from clashing on different pages in the app.
+const fkType = Object.keys(fk)[0]
+
+// Retrieve buttons state and counter state from local storage.
+const buttonsLocalStorageState = JSON.parse(localStorage.getItem(`buttonsState-for-${fkType}`))
+const buttonsClicksLocalStorage = JSON.parse(localStorage.getItem(`buttonsClicks-for-${fkType}`))
+
 const ButtonTestContainer = () => {
+  const [buttonsState, setButtonsState] = useState(buttonsLocalStorageState || initialState)
+  const [count, setCount] = useState(buttonsClicksLocalStorage || 0)
+
+  const handleSetCount = ({ id, isOn }) => {
+    const newCount = isOn ? count - 1 : count + 1
+    const newButtonsState = buttonsState.map(button => {
+      if (button.id === id) {
+        button.isOn = !isOn
+        button.fk = button.isOn ? fk : null
+      }
+      return button
+    })
+
+    localStorage.setItem(`buttonsState-for-${fkType}`, JSON.stringify(newButtonsState))
+    localStorage.setItem(`buttonsClicks-for-${fkType}`, JSON.stringify(newCount))
+
+    setCount(newCount)
+    setButtonsState(newButtonsState)
+  }
+
   return (
     <div className='button-test-container'>
-      <FavoriteButtonWithState />
+      {buttonsState.map(button => (
+        <FavoriteButtonWithState
+          key={button.id}
+          id={button.id}
+          count={count}
+          handleSetCount={handleSetCount}
+          fk={button.fk}
+          defaultOn={button.isOn}
+        />
+      ))}
     </div>
   )
 }
--- src/components/FavoriteButtonWithState/index.jsx
+++ src/components/FavoriteButtonWithState/index.jsx
@@ -1,61 +1,34 @@
-import React, { useState } from 'react'
-
+import { useState } from 'react'
 import FavoriteButton from 'components/FavoriteButton'
 
-// Foreign key type can be changed to 'userId', 'postId', 'commentId', etc...
-const fk = { userId: '1' }
+// Change this value to true to show the foreign key.
+const showForeignKey = true
 
-// Initital state uses time as an fk for this example, but it can be any other value.
-const buttonsInittialState = [
-  { key: 1, isClicked: false, fk: null },
-  { key: 2, isClicked: false, fk: null },
-  { key: 3, isClicked: false, fk: null },
-]
+//  Since foeign key is passed FavoriteButtonWithState component might as well show it.
+const ForeignKey = ({ fk }) => {
+  if (!fk || !showForeignKey) return null
+  const foreignKeyType = Object.keys(fk)[0]
+  const foreignKeyValue = fk[foreignKeyType]
 
-// Need this to prevent buttons states from clashing on different pages in the app.
-const fkType = Object.keys(fk)[0]
+  return (
+    <p>
+      {foreignKeyType}: {foreignKeyValue}
+    </p>
+  )
+}
 
-// Retrieve buttons state and counter state from local storage.
-const buttonsLocalStorageState = JSON.parse(localStorage.getItem(`buttonsState-for-${fkType}`))
-const buttonsClicksLocalStorage = JSON.parse(localStorage.getItem(`buttonsClicks-for-${fkType}`))
+const FavoriteButtonWithState = ({ count, id, handleSetCount, fk, defaultOn }) => {
+  const [isOn, setIsOn] = useState(defaultOn || false)
 
-const FavoriteButtonWithState = () => {
-  const [count, setCount] = useState(buttonsClicksLocalStorage || 0) // If there is no clicks in local storage, set count to 0.
-  const [buttonsState, setButtonsState] = useState(buttonsLocalStorageState || buttonsInittialState) // If there is no buttons state in local storage, set buttons state to inittial state.
-
-  const onClick = buttonKey => {
-    // Create new buttons state.
-    const newButtonsState = buttonsState.reduce((newState, button) => {
-      if (button.key === buttonKey) {
-        return [...newState, { ...button, isClicked: !button.isClicked, fk: button.isClicked ? null : fk }]
-      }
-      return [...newState, button]
-    }, [])
-
-    // Create new count state based on new buttons state.
-    const newCountState = newButtonsState.reduce((total, button) => {
-      if (button.isClicked) {
-        return total + 1
-      }
-      return total
-    }, 0)
-
-    // Store new buttons state and counter state in local storage.
-    localStorage.setItem(`buttonsState-for-${fkType}`, JSON.stringify(newButtonsState))
-    localStorage.setItem(`buttonsClicks-for-${fkType}`, JSON.stringify(newCountState))
-
-    // Update the react states.
-    setCount(newCountState)
-    setButtonsState(newButtonsState)
-
-    // Note: "git checkout cf9dd9b8d3df37d33f8024bbc4fc2c4e8b7ebd6a" to see the other way to update the state (doesn't include 2nd extra credit).
+  const onClick = () => {
+    handleSetCount({ id, isOn })
+    setIsOn(!isOn)
   }
 
   return (
     <>
-      {buttonsState.map(button => (
-        <FavoriteButton key={button.key} on={button.isClicked} count={count} onClick={() => onClick(button.key)} />
-      ))}
+      <ForeignKey fk={fk} />
+      <FavoriteButton on={isOn} count={count} onClick={onClick} />
     </>
   )
 }
